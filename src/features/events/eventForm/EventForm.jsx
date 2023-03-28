7ff803dcd4144491943a26564880c9eea55d4895
Sendy Yagambrum <sendy@novity.io>
2023-03-28 11:38:18 +0400

    Add state

---
+++ src/features/events/eventForm/EventForm.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import { Segment, Header, Form, Button } from "semantic-ui-react";
+
+export default function EventForm({setFormOpen}) {
+    return (
+        <Segment clearing>
+            <Header content='Create new event'/>
+            <Form>
+                <Form.Field>
+                    <input type="text" placeholder="Event Title"/>
+                </Form.Field>
+                <Form.Field>
+                    <input type="text" placeholder="Category"/>
+                </Form.Field>
+                <Form.Field>
+                    <input type="text" placeholder="City"/>
+                </Form.Field>
+                <Form.Field>
+                    <input type="text" placeholder="Venue"/>
+                </Form.Field>
+                <Form.Field>
+                    <input type="date" placeholder="Date"/>
+                </Form.Field>
+                <Button type='submit' float='right' positive content="Submit"/>
+                <Button onClick={() => setFormOpen(false)} type='submit' float='right' content="Cancel"/>
+            </Form>
+        </Segment>
+    )
+}
